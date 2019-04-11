3c2245d4535cf894119607731e2aca0cac05b3c1
jaelyn coles <jaelyncoles@Matthews-MBP-2.localdomain>
2019-04-11 12:36:04 -0400

    got things on screen

---
+++ calc/resources/js/components/calcComponent.vue
@@ -0,0 +1,71 @@
+<template>
+	<div>
+
+			<div id="output">
+				<h2>{{outputnumbutvalue}}</h2>
+                </div>
+                <div class="row">
+				<div class="col-4" v-for="button in buttons" :key="button.id">
+					<numbutComponent
+						v-bind:numbutid="button.id"
+						v-bind:numbutvalue="button.value"
+						v-on:onClickedButton="outputnumbutValue"
+					></numbutComponent>
+				</div>
+                </div>
+                <button id="clear" size="lg" @click="clearAll()">C</button>
+			</div>
+
+</template>
+
+<script>
+	//create a grid for the buttons with div for view
+	//make function that emits which button clicked to the
+	//create string that eval joinby
+	//create a clear button that resets the div
+	
+
+	import numbutComponent from "./numbutComponent";
+
+	export default {
+		name: "calc-component",
+		data() {
+			return {
+				buttons: [
+					{ id: 0, value: 0 },
+					{ id: 1, value: 1 },
+					{ id: 2, value: 2 },
+					{ id: 3, value: 3 },
+					{ id: 4, value: 4 },
+					{ id: 5, value: 5 },
+					{ id: 6, value: 6 },
+					{ id: 7, value: 7 },
+					{ id: 8, value: 8 },
+					{ id: 9, value: 9 },
+					{ id: 10, value: 10 },
+					{ id: 11, value: "+" },
+					{ id: 12, value: "*" },
+					{ id: 13, value: "-" },
+					{ id: 14, value: "/" }
+				],
+				outputnumbutvalue: "                 "
+			};
+		},
+		components: {
+			numbutComponent
+		},
+		methods: {
+			clearAll() {
+				// clear button
+				this.output.value = "";
+			}
+		},
+		computed: {
+			
+			
+        },
+        created() {
+            console.log("we are in the calcc comp now");
+        }
+	};
+</script>
---
+++ calc/resources/js/components/numbutComponent.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<div id="button">
+			<button size="lg" @click="click()">{{ numbutvalue }}</button>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "numbutComponent",
+		props: {
+			numbutid: Number,
+			numbutvalue: String
+		},
+		data() {
+			return {
+				outputnumbutvalue: "",
+				beenClicked: false
+			};
+		},
+		methods: {
+			click() {
+				this.$emit("onClickedButton", { id: this.numbutid });
+			}
+		},
+		computed: {
+			isnumber() {
+				if (isNaN(this.numbutvalue) == false) {
+					this.numbutvalue = parseInt(this.numbutvalue);
+				}
+				return this.numbutvalue;
+			}
+		}
+	};
+</script>
